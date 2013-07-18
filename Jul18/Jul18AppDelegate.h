fd9ec67cd9952c96d4409fc0975c22fc48b35282
Udo Hoppenworth <u.hoppenworth@gmail.com>
2013-07-18 13:04:45 -0400

    Initial Commit

---
+++ Jul18/Jul18AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Jul18AppDelegate.h
+//  Jul18
+//
+//  Created by Udo Hoppenworth on 7/18/13.
+//  Copyright (c) 2013 Udo Hoppenworth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Jul18AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Jul18/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Jul18
+//
+//  Created by Udo Hoppenworth on 7/18/13.
+//  Copyright (c) 2013 Udo Hoppenworth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Jul18AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Jul18AppDelegate class]));
+    }
+}
